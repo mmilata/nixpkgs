39cb19039fbae31657a1cb72f34626fe6f676949
Martin Milata <martin@martinmilata.cz>
2020-03-29 16:54:15 +0200

    dfu-programmer: use libusb-1.0 instead of compat wrapper

--- pkgs/development/tools/misc/dfu-programmer/default.nix
+++ pkgs/development/tools/misc/dfu-programmer/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, libusb }:
+{ stdenv, fetchurl, pkgconfig, libusb1 }:
 let
   version = "0.7.2";
 in
@@ -6,19 +6,18 @@ stdenv.mkDerivation rec {
   pname = "dfu-programmer";
   inherit version;
 
-  buildInputs = [ libusb ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ libusb1 ];
 
   src = fetchurl {
     url = "mirror://sourceforge/dfu-programmer/${pname}-${version}.tar.gz";
     sha256 = "15gr99y1z9vbvhrkd25zqhnzhg6zjmaam3vfjzf2mazd39mx7d0x";
   };
 
-  configureFlags = [ "--disable-libusb_1_0" ];
-
   meta = with stdenv.lib; {
     license = licenses.gpl2;
     description = "A Device Firmware Update based USB programmer for Atmel chips with a USB bootloader";
-    homepage = http://dfu-programmer.sourceforge.net/;
+    homepage = "http://dfu-programmer.sourceforge.net/";
     maintainers = [ maintainers.the-kenny ];
     platforms = platforms.unix;
   };
